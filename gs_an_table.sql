b815bb29b4b24301533dfed4963b2d2dbc57dfa3
Masayuki Tanaka <59272447+gangnamdoll@users.noreply.github.com>
2023-07-01 08:33:19 +0900

    Add files via upload

---
+++ gs_an_table.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- ホスト: localhost
+-- 生成日時: 2023 年 7 月 01 日 01:32
+-- サーバのバージョン： 10.4.28-MariaDB
+-- PHP のバージョン: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- データベース: `gs_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `gs_an_table`
+--
+
+CREATE TABLE `gs_an_table` (
+  `id` int(12) NOT NULL,
+  `name` varchar(64) NOT NULL,
+  `url` varchar(128) NOT NULL,
+  `content` text NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- テーブルのデータのダンプ `gs_an_table`
+--
+
+INSERT INTO `gs_an_table` (`id`, `name`, `url`, `content`, `date`) VALUES
+(9, 'aa', '', '', '2023-06-26 00:00:00'),
+(11, 'あ', 'あ', 'あ', '2023-06-26 00:00:00'),
+(12, 'あ', 'あ', '\r\nあ', '2023-06-26 00:00:00'),
+(13, 'ああ', 'ああいう', 'あ', '2023-06-26 00:00:00');
+
+--
+-- ダンプしたテーブルのインデックス
+--
+
+--
+-- テーブルのインデックス `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
+--
+
+--
+-- テーブルの AUTO_INCREMENT `gs_an_table`
+--
+ALTER TABLE `gs_an_table`
+  MODIFY `id` int(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
